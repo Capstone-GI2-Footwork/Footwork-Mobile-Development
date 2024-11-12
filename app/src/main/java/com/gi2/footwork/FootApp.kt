2b9f0dc444eaddc654f6bc8c3e58750d721686e4
yarabramasta <yarabram111@gmail.com>
2024-11-12 11:58:40 +0700

    chore: code clean up

--- app/src/main/java/com/gi2/footwork/FootApp.kt
+++ app/src/main/java/com/gi2/footwork/FootApp.kt
@@ -7,12 +7,11 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class FootApp : Application(){
-    override fun onCreate() {
-        super.onCreate()
+  override fun onCreate() {
+    super.onCreate()
 
-        MapsInitializer.initialize(applicationContext)
-//        Places.initialize(this, BuildConfig.MAPS_API_KEY)
-        FirebaseApp.initializeApp(this)
-
-    }
+    MapsInitializer.initialize(applicationContext)
+    // Places.initialize(this, BuildConfig.MAPS_API_KEY)
+    FirebaseApp.initializeApp(this)
+  }
 }
