87ed5506c8efc177776787e3f72e712dbcd2ea53
yarabramasta <yarabram111@gmail.com>
2024-11-18 23:14:06 +0700

    style(onboarding): finish slicing onboarding screen

--- app/src/main/java/com/gi2/footwork/ui/composables/atoms/BrandButton.kt
+++ app/src/main/java/com/gi2/footwork/ui/composables/atoms/BrandButton.kt
@@ -1,4 +1,4 @@
-package com.gi2.footwork.presentation.ui.composables
+package com.gi2.footwork.ui.composables.atoms
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
@@ -7,7 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.gi2.footwork.presentation.ui.theme.*
+import com.gi2.footwork.ui.theme.*
 
 @Composable
 fun BrandButton(
