831934c895b68367a521de0a209936279eb365b3
yarabramasta <yarabram111@gmail.com>
2024-11-19 00:12:57 +0700

    style(splash): setup material3 theme for xml

--- app/src/main/java/com/gi2/footwork/MainActivity.kt
+++ app/src/main/java/com/gi2/footwork/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.*
-import com.gi2.footwork.ui.composables.screens.*
+import com.gi2.footwork.ui.composables.screens.FootworkNavHost
 import com.gi2.footwork.ui.theme.FootworkTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,46 +15,9 @@ class MainActivity : ComponentActivity() {
     enableEdgeToEdge()
 
     setContent {
-      val navController = rememberNavController()
-
       FootworkTheme {
-        NavHost(
-          navController,
-          startDestination = FootworkRoute.Index,
-          enterTransition = { EnterTransition.None },
-          exitTransition = { ExitTransition.None }
-        ) {
-          addIndex(navController)
-          addOnboarding(navController)
-        }
+        FootworkNavHost()
       }
     }
-  }
-}
-
-private fun NavGraphBuilder.addIndex(navController: NavController) {
-  composable<FootworkRoute.Index> {
-    IndexScreen(
-      onNavigateToNextScreen = {
-        navController.navigate(FootworkRoute.Onboarding) {
-          popUpTo(FootworkRoute.Onboarding) {
-            inclusive = true
-          }
-        }
-      }
-    )
-  }
-}
-
-private fun NavGraphBuilder.addOnboarding(navController: NavController) {
-  composable<FootworkRoute.Onboarding> {
-    OnboardingScreen(
-      onSignIn = {
-        navController.navigate(FootworkRoute.SignIn)
-      },
-      onSignUp = {
-        navController.navigate(FootworkRoute.SignUp)
-      }
-    )
   }
 }
