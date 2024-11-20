f06f760ba1565053387bfbd9f2f3ca7ec5c58e0a
yarabramasta <yarabram111@gmail.com>
2024-11-20 17:44:20 +0700

    fix(IndexScreen): lifting navController & collect side effect, state outside screen composable

--- app/src/main/java/com/gi2/footwork/ui/composables/screens/IndexScreen.kt
+++ app/src/main/java/com/gi2/footwork/ui/composables/screens/IndexScreen.kt
@@ -2,7 +2,8 @@ package com.gi2.footwork.ui.composables.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
@@ -12,44 +13,22 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.gi2.footwork.FootworkRoute
 import com.gi2.footwork.R
 import com.gi2.footwork.ui.common.UiStatus
 import com.gi2.footwork.ui.theme.FootworkTheme
-import com.gi2.footwork.ui.viewmodel.auth.AuthSideEffect
-import com.gi2.footwork.ui.viewmodel.auth.AuthViewModel
+import com.gi2.footwork.ui.viewmodel.auth.AuthState
 import kotlinx.coroutines.delay
-import org.orbitmvi.orbit.compose.collectAsState
-import org.orbitmvi.orbit.compose.collectSideEffect
 
 @Composable
 fun IndexScreen(
   modifier: Modifier = Modifier,
-  navController: NavController,
-  viewModel: AuthViewModel,
+  onNavigate: () -> Unit,
+  state: AuthState,
 ) {
-  val state by viewModel.collectAsState()
-
-  viewModel.collectSideEffect { effect ->
-    when (effect) {
-      is AuthSideEffect.OnNavigate -> {
-        navController.navigate(effect.route) {
-          popUpTo(FootworkRoute.Index) {
-            inclusive = true
-          }
-        }
-      }
-    }
-  }
-
   LaunchedEffect(state) {
     delay(1000L)
     if (state.status !is UiStatus.Loading) {
-      viewModel.redirect(
-        if (state.isAuthed) FootworkRoute.Home
-        else FootworkRoute.Onboarding
-      )
+      onNavigate()
     }
   }
 
