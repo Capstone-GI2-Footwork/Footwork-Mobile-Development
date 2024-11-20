81607befd09a6e5f49c333db0dcd57035b440b3a
yarabramasta <yarabram111@gmail.com>
2024-11-20 17:44:20 +0700

    fix(IndexScreen): lifting navController & collect side effect, state outside screen composable

--- app/src/main/java/com/gi2/footwork/MainActivity.kt
+++ app/src/main/java/com/gi2/footwork/MainActivity.kt
@@ -10,14 +10,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.*
 import com.gi2.footwork.ui.composables.common.scopedViewModel
 import com.gi2.footwork.ui.composables.screens.*
 import com.gi2.footwork.ui.theme.FootworkTheme
+import com.gi2.footwork.ui.viewmodel.auth.AuthSideEffect
 import com.gi2.footwork.ui.viewmodel.auth.AuthViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.compose.collectSideEffect
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -35,14 +41,7 @@ class MainActivity : ComponentActivity() {
           enterTransition = { EnterTransition.None },
           exitTransition = { ExitTransition.None }
         ) {
-          composable<FootworkRoute.Index> {
-            val viewModel: AuthViewModel = it.scopedViewModel(navController)
-
-            IndexScreen(
-              navController = navController,
-              viewModel = viewModel
-            )
-          }
+          addIndex(navController)
           composable<FootworkRoute.Onboarding> {
             OnboardingScreen(
               navController = navController,
@@ -67,8 +66,41 @@ class MainActivity : ComponentActivity() {
   }
 }
 
+private fun NavGraphBuilder.addIndex(
+  navController: NavController,
+) {
+  composable<FootworkRoute.Index> {
+    val viewModel: AuthViewModel = it.scopedViewModel(navController)
+    val state by viewModel.collectAsState()
+
+    viewModel.collectSideEffect { effect ->
+      when (effect) {
+        is AuthSideEffect.OnNavigate -> {
+          navController.navigate(effect.route) {
+            popUpTo(FootworkRoute.Index) {
+              inclusive = true
+            }
+          }
+        }
+      }
+    }
+
+    IndexScreen(
+      state = state,
+      onNavigate = {
+        viewModel.redirect(
+          if (state.isAuthed) FootworkRoute.Home
+          else FootworkRoute.Onboarding
+        )
+      },
+    )
+  }
+}
+
 @Composable
 private fun HomeScreenFallback() {
+  // TODO: later will be implemented by Juan
+
   Scaffold(
     modifier = Modifier.fillMaxSize()
   ) { innerPadding ->
