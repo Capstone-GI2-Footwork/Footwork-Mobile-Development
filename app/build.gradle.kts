bbadadcb0449c91b6ac795c270660d2637ac1a8d
mohammad.firmansyah <firmansmoh842@gmail.com>
2024-12-08 00:24:44 +0700

    fix: build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,23 @@
-//import java.util.Properties
+import java.util.Properties
+
+val keystoreFile = rootProject.file("app/secrets.properties")
+val properties = Properties()
+if (keystoreFile.exists()) {
+    keystoreFile.inputStream().use {
+        properties.load(it)
+    }
+}
 
 plugins {
-  alias(libs.plugins.androidApplication)
-  alias(libs.plugins.jetbrainsKotlinAndroid)
-  alias(libs.plugins.compose.compiler)
-  alias(libs.plugins.googleKsp)
-  alias(libs.plugins.daggerHiltPlugin)
-  alias(libs.plugins.googleMapsPlatform)
-  id("kotlin-parcelize")
-  alias(libs.plugins.googleGmsGoogleServices)
-  alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.googleKsp)
+    alias(libs.plugins.daggerHiltPlugin)
+    alias(libs.plugins.googleMapsPlatform)
+    id("kotlin-parcelize")
+    alias(libs.plugins.googleGmsGoogleServices)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 //composeCompiler {
@@ -18,134 +26,157 @@ plugins {
 //}
 
 android {
-  namespace = "com.gi2.footwork"
-  compileSdk = 35
+    namespace = "com.gi2.footwork"
+    compileSdk = 35
 
-  defaultConfig {
-    applicationId = "com.gi2.footwork"
-    minSdk = 24
-    //noinspection OldTargetApi
-    targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
+    defaultConfig {
+        applicationId = "com.gi2.footwork"
+        minSdk = 24
+        //noinspection OldTargetApi
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    vectorDrawables {
-      useSupportLibrary = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        fun loadConfig(type: String, key: String, defaultValue: String) {
+            val value = properties.getProperty(key) ?: defaultValue
+            manifestPlaceholders[key] = value
+            buildConfigField(type, key, "\"$value\"")
+        }
+        loadConfig("String", "MAPS_API_KEY", "")
+        loadConfig("String", "FOOTWORK_API_BASE_URL", "")
+
+        manifestPlaceholders["MAPS_API_KEY"] = loadConfig("String", "FOOTWORK_API_BASE_URL", "")
     }
 
-    //load the values from .properties file
-//        val keystoreFile = project.rootProject.file("secrets.properties")
-//        val properties = Properties()
-//        properties.load(keystoreFile.inputStream())
-//
-//        //return empty key in case something goes wrong
-//        val apiKey = properties.getProperty("MAPS_API_KEY") ?: ""
-//        buildConfigField("String", "MAPS_API_KEY","\"${properties.getProperty("MAPS_API_KEY")}\"")
-//        manifestPlaceholders["MAPS_API_KEY"] = apiKey
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_17.toString()
-  }
-  buildFeatures {
-    compose = true
-  }
-  composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.1"
-  }
-  packaging {
-    resources {
-      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-  }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
 
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation(libs.androidx.activity.compose)
-  implementation(platform(libs.androidx.compose.bom))
-  implementation(libs.androidx.ui)
-  implementation(libs.androidx.ui.graphics)
-  implementation(libs.androidx.ui.tooling.preview)
-  implementation(libs.androidx.material3)
-  implementation(libs.androidx.constraintlayout)
-  implementation(libs.transportation.consumer)
-  implementation(libs.androidx.ui.text.google.fonts)
-  implementation(libs.androidx.compose.material.iconsExtended)
-  testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.espresso.core)
-  androidTestImplementation(platform(libs.androidx.compose.bom))
-  androidTestImplementation(libs.androidx.ui.test.junit4)
-  debugImplementation(libs.androidx.ui.tooling)
-  debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.transportation.consumer)
+    implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.androidx.compose.material.iconsExtended)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 
-  // view model compose
-  implementation(libs.androidx.lifecycle.compose.viewmodel)
-  implementation(libs.androidx.lifecycle.compose.runtime)
+    // view model compose
+    implementation(libs.androidx.lifecycle.compose.viewmodel)
+    implementation(libs.androidx.lifecycle.compose.runtime)
 
-  // coil image library
-  implementation(libs.coil.compose)
+    // coil image library
+    implementation(libs.coil.compose)
 
-  // dagger hilt
-  implementation(libs.hilt.android)
-  ksp(libs.hilt.compiler)
-  ksp(libs.androidx.hilt.compiler)
-  implementation(libs.androidx.hilt.navigation.compose)
+    // dagger hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    ksp(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
-  // navigation compose
-  implementation(libs.androidx.navigation.compose)
+    // navigation compose
+    implementation(libs.androidx.navigation.compose)
 
-  // maps
-  implementation(libs.maps.compose)
-  implementation(libs.maps.compose.utils)
-  implementation(libs.maps.compose.widgets)
-  implementation(libs.play.services.maps)
-  implementation(libs.play.services.location)
-  implementation(libs.android.maps.utils)
+    // maps
+    implementation(libs.maps.compose)
+    implementation(libs.maps.compose.utils)
+    implementation(libs.maps.compose.widgets)
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation(libs.android.maps.utils)
 
-  // permission library
-  implementation(libs.accompanist.permissions)
+    // permission library
+    implementation(libs.accompanist.permissions)
 
-  // Splash API
-  implementation(libs.androidx.core.splashscreen)
+    // Splash API
+    implementation(libs.androidx.core.splashscreen)
 
-  // shared preferences
-  implementation(libs.androidx.datastore.preferences)
+    // shared preferences
+    implementation(libs.androidx.datastore.preferences)
 
-  // socket io
-  implementation(libs.socket.io.client.v200)
+    // socket io
+    implementation(libs.socket.io.client.v200)
 
-  // gson
-  implementation(libs.gson)
+    // gson
+    implementation(libs.gson)
 
-  // places api
-  implementation(libs.places)
-  implementation(libs.firebase.messaging)
-  implementation(platform(libs.firebase.bom))
+    // places api
+    implementation(libs.places)
+    implementation(libs.firebase.messaging)
+    implementation(platform(libs.firebase.bom))
 
-  // markdown reader
-  implementation(libs.compose.markdown)
+    // markdown reader
+    implementation(libs.compose.markdown)
 
-  // kotlinx-serialization
-  implementation(libs.kotlinx.serialization.json)
+    // kotlinx-serialization
+    implementation(libs.kotlinx.serialization.json)
 
-  // google mdc components
-  implementation(libs.google.android.material)
+    // google mdc components
+    implementation(libs.google.android.material)
+
+    // okhttp
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.loggingInterceptor)
+    //  retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.serialization)
+    //  skydoves sandwich
+    implementation(libs.sandwich)
+    implementation(libs.sandwich.retrofit)
+    implementation(libs.sandwich.retrofit.serialization)
+    //  coil3
+    implementation(libs.coil.compose)
+    implementation(libs.coil.okhttp)
+    //  skydoves landscapist
+    implementation(libs.landscapist.coil)
+    //  orbit mvi
+    implementation(libs.orbitMvi.core)
+    implementation(libs.orbitMvi.viewmodel)
+    implementation(libs.orbitMvi.compose)
+    testImplementation(libs.orbitMvi.test)
 }
